01136aa9d498de029c4f07c4827a981bb3e331d5
culechetoo <chaitanya16027@iiitd.ac.in>
2021-06-01 19:22:46 +0530

    Added getValues function in mec iterator.

--- src/main/java/de/tum/in/pet/implementation/meanPayoff/RestrictedMecValueIterator.java
+++ src/main/java/de/tum/in/pet/implementation/meanPayoff/RestrictedMecValueIterator.java
@@ -3,6 +3,8 @@ package de.tum.in.pet.implementation.meanPayoff;
 import de.tum.in.pet.values.Bounds;
 import de.tum.in.probmodels.model.Model;
 import de.tum.in.probmodels.model.RestrictedModel;
+import it.unimi.dsi.fastutil.ints.Int2DoubleOpenHashMap;
+import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
 
 import java.util.HashMap;
 
@@ -10,15 +12,15 @@ public class RestrictedMecValueIterator<M extends Model> {
 
   public final RestrictedModel<M> restrictedModel;
   public final double targetPrecision;
-  public final HashMap<Integer, Integer> values;
+  public final Int2DoubleOpenHashMap values;
 
   public RestrictedMecValueIterator(RestrictedModel<M> restrictedModel, double targetPrecision){
     this.restrictedModel = restrictedModel;
     this.targetPrecision = targetPrecision;
-    this.values = new HashMap<>();
+    this.values = new Int2DoubleOpenHashMap();
   }
 
-  public RestrictedMecValueIterator(RestrictedModel<M> restrictedModel, double targetPrecision, HashMap<Integer, Integer> values){
+  public RestrictedMecValueIterator(RestrictedModel<M> restrictedModel, double targetPrecision, Int2DoubleOpenHashMap values){
     this.restrictedModel = restrictedModel;
     this.targetPrecision = targetPrecision;
     this.values = values;
@@ -30,5 +32,9 @@ public class RestrictedMecValueIterator<M extends Model> {
 
   public Bounds getBounds(){
     return null;
+  }
+
+  public Int2DoubleOpenHashMap getValues(){
+    return this.values;
   }
 }
