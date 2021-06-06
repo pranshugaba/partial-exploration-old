40cb0db8274a65de2ac061492518b6aad47398f5
shibashis <shibashis.guha@ulb.ac.be>
2021-06-06 23:56:34 +0530

    Changes to RestrictedMecValueIterator

--- src/main/java/de/tum/in/pet/implementation/meanPayoff/RestrictedMecValueIterator.java
+++ src/main/java/de/tum/in/pet/implementation/meanPayoff/RestrictedMecValueIterator.java
@@ -17,14 +17,16 @@ public class RestrictedMecValueIterator<M extends Model> {
 
   public final RestrictedModel<M> restrictedModel;
   public final double targetPrecision;
-  public final Int2DoubleOpenHashMap values;
+  public final Int2DoubleOpenHashMap values;  // map of states and total values; the average value can be obtained by dividing by iterCount
   public final RewardGenerator<State> rewardGenerator;
+  private int iterCount;  // number of iterations in value iteration
 
   public RestrictedMecValueIterator(RestrictedModel<M> restrictedModel, double targetPrecision, RewardGenerator<State> rewardGenerator){
     this.restrictedModel = restrictedModel;
     this.targetPrecision = targetPrecision;
     this.values = new Int2DoubleOpenHashMap();
     this.rewardGenerator = rewardGenerator;
+    this.iterCount = 0;
   }
 
   public RestrictedMecValueIterator(RestrictedModel<M> restrictedModel, double targetPrecision, RewardGenerator<State> rewardGenerator,
@@ -33,28 +35,46 @@ public class RestrictedMecValueIterator<M extends Model> {
     this.targetPrecision = targetPrecision;
     this.values = values;
     this.rewardGenerator = rewardGenerator;
-
+    this.iterCount = 0;
   }
 
   public void run(){
-    // IntCollection initialStates = restrictedModel.model().getInitialStates();
-    // int[] initialStates_arr = initialStates.toIntArray();
-    // assert !(initialStates_arr.length == 0);
-    // int currentState = initialStates_arr[0];
-    // Get choices from currentState in restrictedModel
-    // restrictedModel.model() gives the corresponding MarkovDecisionProcess object.
-
     int numStates = restrictedModel.model().getNumStates();
+    double[] diff =new double[numStates];  // This array stores the difference of values for each state between two successive iterations.
 
-    for (int state = 0; state < numStates; state++) {
-      double max_action_value = 0.0;
-      List<Action> actions = restrictedModel.model().getActions(state);
-      for (Action action : actions) {
-        double val = rewardGenerator.transitionReward(new State(state), action) + getActionVal(state, action.distribution());
-
-      }
-
+    for (int state = 0; state < numStates; state++) {  // initialise hashmap values
+      values.put(state, 0.0);
     }
+
+    double max, min;
+    do {
+      Int2DoubleOpenHashMap oldValues = new Int2DoubleOpenHashMap(values);
+      for (int state = 0; state < numStates; state++) {
+        double maxActionValue = 0.0;
+        List<Action> actions = restrictedModel.model().getActions(state);
+        for (Action action : actions) {  // find the value of the state over all actions
+          double val = rewardGenerator.transitionReward(new State(state), action) + getActionVal(state, action.distribution());
+          if (val > maxActionValue) {
+            maxActionValue = val;
+          }
+        }
+        values.put(state, maxActionValue);
+        diff[state] = maxActionValue - oldValues.get(state);
+      }
+      iterCount++;
+
+      max=0.0;
+      min=Double.MAX_VALUE;
+      // finding max and min can be done in log n time; we should have a better implementation
+      for (double v : diff) {
+        if (v > max) {
+          max = v;
+        }
+        if (v < min) {
+          min = v;
+        }
+      }
+    } while ((max-min)/iterCount >= targetPrecision);  // stopping criterion of value iteration
   }
 
   private double getActionVal(int state, Distribution distribution) {
@@ -72,10 +92,8 @@ public class RestrictedMecValueIterator<M extends Model> {
         double successorVal = values.get(successor);
         sum = sum + probability * successorVal;
       }
-
     }
     return sum;
-
   }
 
 
