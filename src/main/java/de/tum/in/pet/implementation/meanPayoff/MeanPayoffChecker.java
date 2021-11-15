9e8f5a8db5a1456fd05a8cf47d5674280ec331e3
pazhamalai <arunpazhamalai@gmail.com>
2021-11-16 03:32:00 +0530

    Created ResultWriter

--- src/main/java/de/tum/in/pet/implementation/meanPayoff/MeanPayoffChecker.java
+++ src/main/java/de/tum/in/pet/implementation/meanPayoff/MeanPayoffChecker.java
@@ -45,59 +45,6 @@ public final class MeanPayoffChecker {
   private static final List<Pair<Long, Bounds>> timeVBound = new ArrayList<>();
   private static final List<String> additionalWriteInfo = new ArrayList<>();
 
-  public static void writeResults(CommandLine commandLine) throws IOException {
-    String filename = "temp.txt";
-    BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
-
-    StringBuilder modelDetails = new StringBuilder();
-
-    Option[] options = commandLine.getOptions();
-
-    for (Option option : options) {
-      String shortOption = option.getOpt();
-      String longOption = option.getLongOpt();
-      if (shortOption!=null && commandLine.hasOption(shortOption)) {
-        modelDetails.append("-").append(shortOption).append(" ");
-        if (option.hasArg()) {
-          modelDetails.append(commandLine.getOptionValue(shortOption)).append(" ");
-        }
-      }
-      else if (longOption!=null && commandLine.hasOption(longOption)) {
-        modelDetails.append("--").append(longOption).append(" ");
-        if (option.hasArg()) {
-          modelDetails.append(commandLine.getOptionValue(longOption)).append(" ");
-        }
-      }
-    }
-
-    writer.write(modelDetails.toString());
-    writer.newLine();
-
-    StringBuilder times = new StringBuilder();
-    StringBuilder lowerBounds = new StringBuilder();
-    StringBuilder upperBounds = new StringBuilder();
-
-    for (Pair<Long, Bounds> timeBounds: timeVBound){
-      times.append(timeBounds.first).append(" ");
-      lowerBounds.append(timeBounds.second.lowerBound()).append(" ");
-      upperBounds.append(timeBounds.second.upperBound()).append(" ");
-    }
-
-    writer.write(times.toString());
-    writer.newLine();
-    writer.write(lowerBounds.toString());
-    writer.newLine();
-    writer.write(upperBounds.toString());
-    writer.newLine();
-
-    for (String info : additionalWriteInfo) {
-      writer.write(info);
-      writer.newLine();
-    }
-
-    writer.close();
-  }
-
   public static double solve(ModelGenerator generator, int rewardIndex, InputValues inputValues)
           throws PrismException {
     ModelType modelType = generator.getModelType();
@@ -258,7 +205,7 @@ public final class MeanPayoffChecker {
     double meanPayoff = solve(generator, rewardIndex, ip);
     long endTime = System.currentTimeMillis();
 
-    writeResults(commandLine);
+    ResultWriter.write(commandLine, timeVBound, additionalWriteInfo);
 
     logger.log(Level.INFO, "Time to parse, construct model, and compute {0}", new Object[] {endTime-startTime1});
     logger.log(Level.INFO, "Time to compute {0}", new Object[] {endTime-startTime2});
--- src/main/java/de/tum/in/pet/implementation/meanPayoff/ResultWriter.java
+++ src/main/java/de/tum/in/pet/implementation/meanPayoff/ResultWriter.java
@@ -1,4 +1,68 @@
 package de.tum.in.pet.implementation.meanPayoff;
 
+import de.tum.in.pet.values.Bounds;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+import prism.Pair;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 public class ResultWriter {
+
+    public static void write(CommandLine commandLine, List<Pair<Long, Bounds>> timeVBound,
+                             List<String> additionalWriteInfo) throws IOException {
+        String filename = "temp.txt";
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+
+        StringBuilder modelDetails = new StringBuilder();
+
+        Option[] options = commandLine.getOptions();
+
+        for (Option option : options) {
+            String shortOption = option.getOpt();
+            String longOption = option.getLongOpt();
+            if (shortOption!=null && commandLine.hasOption(shortOption)) {
+                modelDetails.append("-").append(shortOption).append(" ");
+                if (option.hasArg()) {
+                    modelDetails.append(commandLine.getOptionValue(shortOption)).append(" ");
+                }
+            }
+            else if (longOption!=null && commandLine.hasOption(longOption)) {
+                modelDetails.append("--").append(longOption).append(" ");
+                if (option.hasArg()) {
+                    modelDetails.append(commandLine.getOptionValue(longOption)).append(" ");
+                }
+            }
+        }
+
+        writer.write(modelDetails.toString());
+        writer.newLine();
+
+        StringBuilder times = new StringBuilder();
+        StringBuilder lowerBounds = new StringBuilder();
+        StringBuilder upperBounds = new StringBuilder();
+
+        for (Pair<Long, Bounds> timeBounds: timeVBound){
+            times.append(timeBounds.first).append(" ");
+            lowerBounds.append(timeBounds.second.lowerBound()).append(" ");
+            upperBounds.append(timeBounds.second.upperBound()).append(" ");
+        }
+
+        writer.write(times.toString());
+        writer.newLine();
+        writer.write(lowerBounds.toString());
+        writer.newLine();
+        writer.write(upperBounds.toString());
+        writer.newLine();
+
+        for (String info : additionalWriteInfo) {
+            writer.write(info);
+            writer.newLine();
+        }
+
+        writer.close();
+    }
 }
