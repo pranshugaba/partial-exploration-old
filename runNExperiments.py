d5019026845abd99e6023ee41944aa0c56d798c1
pazhamalai <arunpazhamalai@gmail.com>
2021-12-21 05:48:20 +0530

    Bug fixes in runNExperiments.py

--- runNExperiments.py
+++ runNExperiments.py
@@ -59,7 +59,7 @@ def get_average_values(model_result_list):
     for model_result in model_result_list:
         average_lower_bound += model_result.lower_bounds[-1]
         average_upper_bound += model_result.upper_bounds[-1]
-        average_run_time += model_result.get_runtime
+        average_run_time += model_result.get_runtime()
 
     average_lower_bound /= len(model_result_list)
     average_upper_bound /= len(model_result_list)
@@ -78,23 +78,25 @@ def write_model_result(result_file, model_result):
     result_file.write('\n')
 
 
-def write_model_results(model_name, model_result_list):
-    result_file_name = model_name + '.txt'
+def write_model_results(model_name, model_result_list, result_directory):
+    result_file_name = result_directory + model_name + '.txt'
 
     with open(result_file_name, 'w') as result_file:
         for model_result in model_result_list:
             write_model_result(result_file, model_result)
 
         (al, au, ar) = get_average_values(model_result_list)
+        precision = au - al
         result_file.write('Average Lower Bound: ' + str(al) + '\n')
         result_file.write('Average Upper Bound: ' + str(au) + '\n')
         result_file.write('Average Run time: ' + str(ar) + '\n')
+        result_file.write('Precision: ' + str(precision) + '\n')
 
 
-def write_results(results):
+def write_results(results, result_directory):
     for model_name, model_result_list in results.items():
         model_result_list.sort(key=result_comparator)
-        write_model_results(model_name, model_result_list)
+        write_model_results(model_name, model_result_list, result_directory)
 
 
 def remove_old_results():
@@ -109,4 +111,4 @@ remove_old_results()
 exec_command = get_exec_command_from_input()
 run_benchmarks(10, exec_command, input_values.output_directory)
 benchmarkInfo = accumulate_results()
-write_results(benchmarkInfo)
+write_results(benchmarkInfo, resultDirectory)
