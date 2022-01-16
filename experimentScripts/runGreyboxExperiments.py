abb7c38f09eff931fd96d9e883cd821dbd554d3c
pazhamalai <arunpazhamalai@gmail.com>
2022-01-16 17:27:02 +0530

    Scripts for running greybox experiments

--- experimentScripts/runGreyboxExperiments.py
+++ experimentScripts/runGreyboxExperiments.py
@@ -60,10 +60,11 @@ def configuration_6():
     return Configuration(inputOptions.simulate_mec_cheat, inputOptions.deltat_method_max_successors, output_directory)
 
 
-all_configurations = [configuration_1(), configuration_2(), configuration_3(), configuration_4(), configuration_5(), configuration_6()]
-
-
-pool = Pool(processes=3)
-pool.map(run_configuration, all_configurations)
-pool.close()
-pool.join()
+run_configuration(configuration_2())
+# all_configurations = [configuration_1(), configuration_2(), configuration_3(), configuration_4(), configuration_5(), configuration_6()]
+#
+#
+# pool = Pool(processes=3)
+# pool.map(run_configuration, all_configurations)
+# pool.close()
+# pool.join()
